a51b98f5219a1434f1ee4585f7580612323c0eb0
aditya140896 <92623400+aditya140896@users.noreply.github.com>
2021-10-16 22:48:34 +0530

    Check

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -5,5 +5,9 @@ class StringCalculator {
     public int add(String input) {
         return 0;
     }
-
+    public int add(String input1, String input2) {
+    	int i1= Integer.parseInt(input1);
+    	int i2= Integer.parseInt(input2);
+    	return i1+i2;
+    }
 }
--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -17,4 +17,10 @@ class StringCalculatorShould {
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(1, stringCalculator.add("1"));
     }
+    
+    @Test
+    void string_with_single_number_should_return_number_withDelimiter() {
+        StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(1, stringCalculator.add("1,2"));
+    }
 }
